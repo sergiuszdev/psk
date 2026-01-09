595b78bf064683d929252ad953951509b0440c93
KacperK02 <104159134+KacperK02@users.noreply.github.com>
2026-01-09 16:15:18 +0100

    dataset2

--- model/Model.py
+++ model/Model.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import StandardScaler, RobustScaler
 import numpy as np
-from data_load import prepare_datasets
+from data_load import prepare_datasets, load_dataset1
 
 class NetworkAutoencoder(nn.Module):
     def __init__(self, input_dim):
@@ -45,10 +45,11 @@ class NetworkAutoencoder(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
 
+
 def main():
     print("--- Wczytywanie danych ---")
 
-    (X_train_df, _), (X_test_df, y_test_series) = prepare_datasets()
+    (X_train_df, _), (X_test_df, y_test_series) = load_dataset1()
 
     print(f"Dane treningowe (Norma): {X_train_df.shape}")
     print(f"Dane testowe (Mix): {X_test_df.shape}")
@@ -60,7 +61,7 @@ def main():
     scaler = RobustScaler() # skalowanie wokół mediany
     X_train_scaled = scaler.fit_transform(X_train_values)
     X_test_scaled = scaler.transform(X_test_values)
-
+    
     # Konwersja na Tensory PyTorch
     train_tensor = torch.FloatTensor(X_train_scaled)
     test_tensor = torch.FloatTensor(X_test_scaled)
@@ -134,7 +135,7 @@ def main():
         
         recall = tp / (tp + fn) if (tp + fn) > 0 else 0
         precision = tp / (tp + fp) if (tp + fp) > 0 else 0
-        f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        f1 = (2 * tp) / (2 * tp + fp + fn) if 2 * tp + fp + fn > 0 else 0
         
         print(f"{p:<10} | {threshold_temp:.6f} | {recall*100:6.2f}% | {precision*100:6.2f}% | {f1:.4f} | {fp} | {tp}")
 
